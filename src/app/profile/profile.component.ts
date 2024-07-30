7a1c9fa2378d231b2d66c902bcd4dbf38768496a
Francesca Piccitto <framari.p@gmail.com>
2024-07-30 18:00:38 +0200

    inizia a implementare il profilo

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,23 +4,34 @@ import { User } from '../model/user.model';
 import { Item } from '../model/item.model';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTabsModule} from '@angular/material/tabs';
+import { ItemType } from '../model/itemtype.model';
+import { WishlistService } from '../services/wishlist.service';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatTabsModule, MatIconModule],
+  imports: [MatTabsModule, MatIconModule, MatGridListModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent implements OnInit{
+  flipped = false;
   user : User|undefined;
   items: Item[] = [];
+  myWishlist : ItemType[] = [];
+  
 
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private wishlistService: WishlistService) {}
 
   ngOnInit(): void {
     this.userService.getUserDetails().subscribe(u => this.user = u);
     this.userService.getItems().subscribe((i : Item[]) => (this.items=i));
+    this.wishlistService.getMyWishlist().subscribe((it: ItemType[]) => {this.myWishlist = it;})
+  }
+  
+  toggleFlip() {
+    this.flipped = !this.flipped;
   }
 
 }
